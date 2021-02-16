09dac731d110cbf399ec8b1938775e222e2c16e5
Tristan Singleton <tksingle@mtu.edu>
2021-02-15 19:16:29 -0500

    Imported Project structure to IntelliJ Project structure

---
+++ frontend/src/UI/MainApp.java
@@ -0,0 +1,36 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        // load UI resources
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane root = new AnchorPane();
+
+        Scene scene = new Scene(root, 300, 300, Color.GRAY);
+
+        stage.setTitle("MasterPlan");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        // release UI resources
+    }
+}
