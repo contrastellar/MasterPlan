14075bb7bc4b690d2cd40c111da8579b16a9db3e
Tristan Singleton <tksingle@mtu.edu>
2021-02-18 21:19:14 -0500

    Tag Override equas/hashCode. Added Archivable to Workspace.

--- frontend/src/components/workspaces/Workspace.java
+++ frontend/src/components/workspaces/Workspace.java
@@ -1,8 +1,16 @@
 package components.workspaces;
 
 import MVVM.Observable;
+import components.Archivable;
 
-public abstract class Workspace {
+public abstract class Workspace implements Archivable {
+
     public final Observable<String> name = new Observable<>("");
+    public final Observable<Boolean> isArchived = new Observable<>(false);
+
+    public Workspace() { }
+
+    public boolean isArchived() { return isArchived.getValue(); }
+    public void setArchive(boolean isArchived) { this.isArchived.setValue(isArchived); }
 
 }
