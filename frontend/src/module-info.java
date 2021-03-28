e65a8968646cf5efe31efa0e30a8497f7266abd8
Tristan Singleton <tksingle@mtu.edu>
2021-03-28 15:32:36 -0400

    previous commit doesn't compile; fixed issues.

--- frontend/src/module-info.java
+++ frontend/src/module-info.java
@@ -2,7 +2,6 @@ module MasterPlan {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires org.apache.commons.collections4;
 
     opens UI to javafx.fxml;
     opens UI.view to javafx.fxml;
