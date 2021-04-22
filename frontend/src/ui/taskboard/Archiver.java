bf98e04c5f394a2463f301420f999ac2873bc782
Gabby <gjstimac@gmail.com>
2021-04-21 21:05:36 -0500

    Created Archiver -- Container for the fuckshit I'm about to attempt

---
+++ frontend/src/ui/taskboard/Archiver.java
@@ -0,0 +1,10 @@
+package ui.taskboard;
+
+/**
+ * This is an object used to store
+ * 1) Task information
+ * 2) The vertex object in ListView
+ * 3) The vertex object in the Archive
+ */
+public class Archiver {
+}
