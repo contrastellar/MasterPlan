fb86b152c1fb8354a39ab19dbb71ebd3f9377e70
Daniel Henderson <dphender@mtu.edu>
2021-04-22 13:11:15 -0600

    HELP

--- frontend/src/module-info.java
+++ frontend/src/module-info.java
@@ -3,20 +3,21 @@ module MasterPlan {
     requires javafx.fxml;
     requires javafx.graphics;
 
-    opens ui                             to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.workspaces.archivespace to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.account.settings            to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.account.settings.menu       to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.account.login               to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.account.login.create        to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.util.icon                 to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.workspaces to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.workspaces.editbar           to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.window                      to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.window.statusbar            to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.window.toolbar              to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.window.toolbar.tab          to javafx.graphics, javafx.controls, javafx.fxml;
-    opens ui.workspaces.listspace to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui                               to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.tag                           to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.workspaces.archivespace       to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.account.settings              to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.account.settings.menu         to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.account.login                 to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.account.login.create          to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.util.icon                     to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.workspaces                    to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.workspaces.editbar            to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.window                        to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.window.statusbar              to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.window.toolbar                to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.window.toolbar.tab            to javafx.graphics, javafx.controls, javafx.fxml;
+    opens ui.workspaces.listspace          to javafx.graphics, javafx.controls, javafx.fxml;
     opens ui.workspaces.listspace.category to javafx.graphics, javafx.controls, javafx.fxml;
     opens ui.workspaces.listspace.task     to javafx.graphics, javafx.controls, javafx.fxml;
 }
